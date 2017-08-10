5d358dff6b2b1b409842d55a5168793574a27b5a
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2017-08-10 11:09:21 +0200

    ReadmeExample changed flag to --filter and now it accepts arguments, which are non-flags

--- src/main/java/com/github/yin/flags/example/ReadmeExample.java
+++ src/main/java/com/github/yin/flags/example/ReadmeExample.java
@@ -9,18 +9,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * Reads file and prints it's output. This is an example of java-flags use.
+ * Reads files and prints them word by word filtering them by a list of allowed words.
+ * This is an example of java-flags use.
  */
-@FlagDesc("This class is an example how to print files")
+@FlagDesc("This class is an example how to print files and filter them by list of words. "
+        + "Usage: java com.github.yin.flags.example.ReadmeExample "
+        + "[--filter <list-of-words>] <input1> <input2> ...")
 public class ReadmeExample {
     /**
      * This file must be readable
      */
-    @FlagDesc("Specifies path to input file")
-    static final Flag<String> input = Flags.create("")
+    @FlagDesc("Path to file containing a list of words to filter the inputs by")
+    static final Flag<String> filter = Flags.create("")
             .validator((String path) -> {
                 if (path == null || path.isEmpty()) {
                     throw new Flags.ParseException("Input path is empty");
@@ -31,52 +35,59 @@ public class ReadmeExample {
                 }
             });
 
-    private final String inputfile;
+    private final Path filterFile;
 
     public static void main(String[] args) {
+        List<String> nonFlags;
         try {
-            List<String> nonFlags = Flags.parse(args, ImmutableList.of("com.github.yin.flags.example"));
-            if (nonFlags.size() > 0) {
-                System.err.println("No arguments except flags are supported by this program");
-                Flags.printUsage("com.github.yin.flags.example");
-                System.exit(1);
-                return;
-            }
+            // List<String> nonFlags = Flags.parse(args, ImmutableList.of("com.github.yin.flags.example"));
+            nonFlags = Flags.parse(args, ImmutableList.of("com.github.yin.flags.example"));
         } catch (Flags.ParseException e) {
-            System.err.println(e.getMessage());
-            Flags.printUsage("com.github.yin.flags.example");
-            System.exit(1);
+            printUsageAndExit(e.getMessage());
+            return;
+        }
+        if (nonFlags.size() == 0) {
+            printUsageAndExit("No input files specified in arguments");
             return;
         }
         // TODO yin: Default value is not validated, implement required flags
-        if (input.get().isEmpty()) {
-            System.err.println("--input is empty");
-            Flags.printUsage("com.github.yin.flags.example");
-            System.exit(1);
-            return;
-        }
         ReadmeExample re = new ReadmeExample();
-        re.run();
+        for (String a : nonFlags) {
+            re.process(Paths.get(a));
+        }
+    }
+
+    private static void printUsageAndExit(String message) {
+        System.err.println(message);
+        Flags.printUsage("com.github.yin.flags.example");
+        System.exit(1);
     }
 
     /**
      * Creates instance using java-flag provided values.
      */
     ReadmeExample() {
-        this.inputfile = input.get();
+        this.filterFile = Paths.get(filter.get());
     }
 
     /**
      * Creates instance supplied by external values.
      */
-    ReadmeExample(String inputfile) {
-        this.inputfile = inputfile;
+    ReadmeExample(Path inputfile) {
+        this.filterFile = inputfile;
     }
 
-    public void run() {
-        Path path = Paths.get(inputfile);
+    public void init() throws IOException {
+        Object[] objects = Files.lines(filterFile)
+                .map(line -> line.split("\\W+"))
+                .flatMap(Arrays::stream)
+                .toArray();
+        System.out.println(objects);
+    }
+
+    public void process(Path input) {
         try {
-            Files.lines(path).forEach(System.out::println);
+            Files.lines(input).forEach(System.out::println);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
